510931d1b4c7cea0d970600295fa044d1b4b15cd
matteichler <matteichler7@gmail.com>
2018-11-08 00:19:40 -0800

    ticketmaster looking not bad

--- client/src/components/Discover/discover.js
+++ client/src/components/Discover/discover.js
@@ -10,7 +10,7 @@ class Discover extends Component {
         }
     }
     componentDidMount() {
-        fetch("https://app.ticketmaster.com/discovery/v2/events.json?size=30&apikey=vApG5Du5K59rloN5BsmgN1rukqRaDEZv&genreId=")
+        fetch("https://app.ticketmaster.com/discovery/v2/events.json?size=30&apikey=vApG5Du5K59rloN5BsmgN1rukqRaDEZv&segmentName=music&city=la")
             .then(res => res.json())
             .then(
                 (result) => {
@@ -31,14 +31,30 @@ class Discover extends Component {
         } else {
             return (
                 <div>
-                <ul>
+                <table>
+                    <tr>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Venue</th>
+                        <th>Date</th>
+                        <th>Tickets</th>
+                    </tr>
                     {events.map(event => (
-                        <li key={event.name}>
-                        {event.name}
-                        </li>
+                    <tr key={event.name}>
+                        <td>
+                            <img src={event.images[0].url} alt="thumbnail" className="thumbnail"/>
+                        </td>
+                        <td>{event.name}</td>
+                        <td>{event.classifications[0].genre.name}</td>
+                        <td>{event._embedded.venues[0].name}</td>
+                        <td>{event.dates.start.localDate}</td>
+                        <td>
+                            <a href={event.url}>Tickets</a>
+                        </td>
+                    </tr>
                     ))}
-                </ul>
-                    <button onClick={this.componentDidMount}>CLICK ME FOR CONSOLE LOGS</button>
+                </table>
                 </div>
             );
         }
